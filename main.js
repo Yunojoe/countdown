9fe803acd398c62fb3552c8809a57ed4437fad96
Yunojoe <joe.ford@yunomedia.com>
2018-07-14 21:15:56 +0100

    added second countdown

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ var countDownDate = new Date("May 2, 2019 13:00:00").getTime();
 var christmas = 11102400000;
 var StartDistance = 32630400000;
 
+var nextevent = new Date("July 20, 2018 00:00:00").getTime();
+
 // get distance between the date and start distance
 // get the percentage 
 // make it a width
@@ -19,6 +21,8 @@ var x = setInterval(function () {
     var elapsed = StartDistance - distance; 
     var Progress = (100 * elapsed) / (StartDistance);
 	
+	var event = nextevent - now;
+	
   // Time calculations for days, hours, minutes and seconds
 	var days = Math.floor(distance / (1000 * 60 * 60 * 24));
 	var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
@@ -30,6 +34,12 @@ var x = setInterval(function () {
 	var elapsed_hours = Math.floor((elapsed % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
 	var elapsed_minutes = Math.floor((elapsed % (1000 * 60 * 60)) / (1000 * 60));
 	var elapsed_seconds = Math.floor((elapsed % (1000 * 60)) / 1000);
+	
+	
+	var event_days = Math.floor(event/ (1000 * 60 * 60 * 24));
+	var event_hours = Math.floor((event % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+	var event_minutes = Math.floor((event % (1000 * 60 * 60)) / (1000 * 60));
+	var event_seconds = Math.floor((event % (1000 * 60)) / 1000);
 
 
   // Display the results
@@ -53,6 +63,12 @@ var x = setInterval(function () {
     document.getElementById("hours").innerHTML = hours;
 	document.getElementById("minutes").innerHTML = minutes;
 	document.getElementById("seconds").innerHTML = seconds;
+	
+	document.getElementById("event_days").innerHTML = event_days;
+    document.getElementById("event_hours").innerHTML = event_hours;
+	document.getElementById("event_minutes").innerHTML = event_minutes;
+	document.getElementById("event_seconds").innerHTML = event_seconds;
+	
 
   // If the count down is finished do this 
   if (distance < 0) {
